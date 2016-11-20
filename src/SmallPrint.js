0657e3bc733531364ed47ccfefc2ddb3c220854b
Matias Korhonen <matias@kiskolabs.com>
2016-11-20 19:44:50 +0200

    Fixes the legalese syntax

--- src/SmallPrint.js
+++ src/SmallPrint.js
@@ -7,7 +7,7 @@ class SmallPrint extends Component {
     return (
       <div className="smallprint">
         <small>
-          Apple Watch® Series 2 images © Copyright 2016 Apple Inc.
+          Apple Watch® Series 2 images: Copyright © 2016 Apple Inc.
           <br />
           By using them you agree to the <a href="https://developer.apple.com/app-store/marketing/guidelines/">
           App Store Marketing Guidelines</a> and the iOS App Store Marketing
